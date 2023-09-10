ff8b86f23ff76491429cbadb2551eaffd0c6ee99
Brent L <brent.livesay@yahoo.com>
2023-09-10 12:01:01 -0400

    Fixed wording and removed button properties on icon

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -2,8 +2,6 @@ import AppBar from "@mui/material/AppBar";
 import Box from "@mui/material/Box";
 import Toolbar from "@mui/material/Toolbar";
 import Typography from "@mui/material/Typography";
-import Button from "@mui/material/Button";
-import IconButton from "@mui/material/IconButton";
 import ChecklistRtlOutlinedIcon from "@mui/icons-material/ChecklistRtlOutlined";
 
 export default function Navbar() {
@@ -11,17 +9,9 @@ export default function Navbar() {
     <Box sx={{ flexGrow: 1 }}>
       <AppBar position="static" sx={{ bgcolor: "#00838F" }}>
         <Toolbar>
-          <IconButton
-            size="large"
-            edge="start"
-            color="inherit"
-            aria-label="menu"
-            sx={{ mr: 2 }}
-          >
-            <ChecklistRtlOutlinedIcon />
-          </IconButton>
-          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-            My Todo List featuring React and MUI
+          <ChecklistRtlOutlinedIcon />
+          <Typography variant="h6" component="div" sx={{ flexGrow: 1, ml: 2 }}>
+            My To-Do List featuring React and MUI
           </Typography>
         </Toolbar>
       </AppBar>
